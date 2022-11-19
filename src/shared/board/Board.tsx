9e3f95afebad2bfbe4907dd2b7b8fa322cdce235
Hafez Kaghazi <smart.hafez@live.com>
2022-11-19 15:35:55 +0330

    update

--- src/shared/board/Board.tsx
+++ src/shared/board/Board.tsx
@@ -41,7 +41,7 @@ export const Board: React.FC<BoardProps> = ({ currentValue, showTime = "onlyDate
         )}
         {(showTime == "both" || showTime == "onlyTime") && (
           <>
-            <span>{currentValue.locale("fa").format("hh:mm a")}</span>
+            <span>{currentValue.locale("fa").format("HH:mm")}</span>
           </>
         )}
       </main>
